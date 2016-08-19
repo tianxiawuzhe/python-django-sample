3dd375f29afba71b416ea57df583a09c1dba202b
tianxiawuzhe <j_l2006@163.com>
2016-08-19 17:47:22 +0800

    Add files via upload

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,2 +1,4 @@
-/code/manage.py syncdb --noinput
+##/code/manage.py syncdb --noinput
+/code/manage.py makemigrations
+/code/manage.py migrate
 /usr/local/bin/gunicorn config.wsgi:application -w 2 -b :8000
